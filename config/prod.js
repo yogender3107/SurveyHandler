747bd40f1a635f831a66a23b8c1f4841825787c8
yogender3107 <yogender3107@gmail.com>
2018-11-25 18:06:41 +0530

    Auth Prod

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,//'249230200191-sglruijkm41fp634roau5bjdo8fnra0h.apps.googleusercontent.com',
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,//'VKa0dsvP1hCUXZCiRSHjqS5d',
+    mongoURI: process.env.MONGO_URI,//'mongodb://yogender3107:ykj317941856@ds115854.mlab.com:15854/emaily-prod',
+    cookieKey: process.env.COOKIE_KEY//'pohkjbhjfhdrtkjlkhudxjhlio'
+}
--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const CookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/User');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send({hi:'there'})
+app.use(CookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' })
 });
 
 const PORT = process.env.PORT || 5000;
