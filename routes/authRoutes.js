4f162f01f52e8295c2585b99ae9b00bf96b684c0
yogender3107 <yogender3107@gmail.com>
2018-11-25 20:11:20 +0530

    logs for test

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,16 @@
 const passport = require('passport');
 
 module.exports = (app) => {
-    app.get('/auth/google', passport.authenticate('google', {
-        scope: ['profile', 'email']
+    app.get('/auth/google', passport.authenticate('google', () => {
+        console.log("inside /auth/google")
+        return {
+            scope: ['profile', 'email']
+        }
     }))
 
-    app.get('/auth/google/callback', passport.authenticate('google'));
+    app.get('/auth/google/callback', passport.authenticate('google', () => {
+        console.log("inside /auth/google/callback");
+    }));
 
     app.get('/api/current_user', (req, res) => {
         res.send(req.user);
