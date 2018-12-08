d83eaa014913e67ad53e5bd15c9c0a919b06a2cb
yogender3107 <yogender3107@gmail.com>
2018-12-08 16:23:22 +0530

    ok

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 authRoutes(app);
 billingRoutes(app);
 
-if (process.env.NODE_ENV = 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
